f88023bfa2fdb10730bb570a1cbebc32ee9229ae
Luciara-Abreu <luciara.abreu@gmail.com>
2023-03-15 10:09:30 -0300

    Segunda Semana - Aula1 -Rrimeiro hello word

---
+++ primeiro-arquivo-node.js
@@ -0,0 +1,17 @@
+console.log("Primeiro Hello Word");
+
+console.log("----------------------------------------------------------------");
+const numero1 = 10;
+const numero2 = 15;
+
+const soma = () => {
+  return numero1 + numero2;
+};
+
+const result = soma();
+
+console.log("O resultado da soma é :", result);
+console.log("----------------------------------------------------------------");
+const date = new Date();
+console.log("A data de hoje é:", date);
+console.log("----------------------------------------------------------------");
