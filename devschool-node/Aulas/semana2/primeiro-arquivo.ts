1c991a82d15b3e66dfdc2b1a9ac4ffd8fd951cab
Luciara-Abreu <luciara.abreu@gmail.com>
2023-03-15 17:48:00 -0300

    class week 2

---
+++ devschool-node/Aulas/semana2/primeiro-arquivo.ts
@@ -0,0 +1,16 @@
+console.log("Eis que surge o primeiro arquivo typescript");
+
+console.log('--------------------------------------------------')
+let n1: number =5
+let n2: number = 10
+
+n1 = 10
+
+console.log(n1 + n2)
+console.log('--------------------------------------------------')
+
+const textoResultado: string = 'Resultado: '
+const result: number = n1+n2
+
+console.log(textoResultado, result)
+console.log('--------------------------------------------------')
