7785d2cde0684d6bacd81be0477b3ae9b6a82b79
Luciara-Abreu <luciara.abreu@gmail.com>
2023-03-20 16:40:01 -0300

    add listAllUsers

--- Agenda_Aniversariante/src/controllers/UserController.ts
+++ Agenda_Aniversariante/src/controllers/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import CreateUserService from '../sercive/userServices/createUserService'
+import ListAllUserService from '../sercive/userServices/listAllUserService'
 
 class UserController {
   public async createUser(req: Request, res: Response): Promise<Response> {
@@ -17,8 +18,13 @@ class UserController {
     return res.json(thisUser)
   }
 
-
-  
+  public async listAllUsers(req: Request, res: Response) {
+    const listUsers = new ListAllUserService()
+    const showUsers = await listUsers.execute()
+    
+    console.log('userController==> ', showUsers)
+    return res.json(showUsers)
+  }
 }
 
 export default UserController
