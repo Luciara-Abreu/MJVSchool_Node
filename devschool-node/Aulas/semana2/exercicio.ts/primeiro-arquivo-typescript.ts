01ea9adc073801894c16298667273bd7582cf596
Luciara-Abreu <luciara.abreu@gmail.com>
2023-03-20 21:14:53 -0300

    Add a soma de 2 + 2

--- devschool-node/Aulas/semana2/exercicio.ts/primeiro-arquivo-typescript.ts
+++ devschool-node/Aulas/semana2/exercicio.ts/primeiro-arquivo-typescript.ts
@@ -1,15 +1,25 @@
+const Soma = () => {
+  const n1 = 2;
+  const n2 = 2;
+
+  const result = n1 + n2;
+  return result;
+};
+
+const soma = Soma();
+
 const execute1 = () => {
-  const printLine: string = "Meu primeiro arquivo typescript1";
-  return console.log(printLine);
+  const printLine: string = "Meu primeiro arquivo typescript1 no node";
+
+  return console.log(printLine, soma);
 };
 
 execute1();
 //--------------------------Ou-----------------------------------
-const informacao: string = "Meu primeiro arquivo typescript2";
+const informacao: string = "Meu primeiro arquivo typescript2 no node";
 
 const execute2 = (informacao: string) => {
   const printLine = informacao;
-  return console.log(printLine);
+  return console.log(printLine, soma);
 };
-
 execute2(informacao);
