76f0cbd5b0af9fbbf310a63421cc9cce8f46fe1e
Luciara-Abreu <luciara.abreu@gmail.com>
2023-03-21 19:24:50 -0300

    Updates

--- devschool-node/Aulas/Semana2/Video1/exercicios/primeiro-arquivo.ts
+++ devschool-node/Aulas/Semana2/Video1/exercicios/primeiro-arquivo.ts
@@ -1,16 +1,16 @@
 console.log("Eis que surge o primeiro arquivo typescript");
 
-console.log('--------------------------------------------------')
-let n1: number =5
-let n2: number = 10
+console.log("--------------------------------------------------");
+let n1: number = 5;
+let n2: number = 10;
 
-n1 = 10
+n1 = 10;
 
-console.log(n1 + n2)
-console.log('--------------------------------------------------')
+console.log(`A soma de ${n1} + ${n2} é =`, n1 + n2);
+console.log("--------------------------------------------------");
 
-const textoResultado: string = 'Resultado: '
-const result: number = n1+n2
+const textoResultado: string = `A soma de ${n1} + ${n2} é =`;
+const result: number = n1 + n2;
 
-console.log(textoResultado, result)
-console.log('--------------------------------------------------')
+console.log(textoResultado, result);
+console.log("--------------------------------------------------");
