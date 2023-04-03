0872f0095c5f580bc8b59e870144479257cbe430
Luciara-Abreu <luciara.abreu@gmail.com>
2023-04-03 12:18:05 -0300

    user routes

--- Agenda_Aniversariante/src/routes/userRoutes/userRoutes.ts
+++ Agenda_Aniversariante/src/routes/userRoutes/userRoutes.ts
@@ -10,7 +10,10 @@ console.log('✨ ********** Rotas de User *************** ✨')
 console.log('✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨')
 console.log('')
 
-route.post('/AddUser', userController.createUser)                                                   
+route.post('/AddUser', userController.createUser)
 route.get('/ListAllUsers', userController.listAllUsers)
+route.get('/ListOne/:id', userController.listOneUser)
+route.patch('/Update/:id', userController.updateUser)
+route.delete('/Remove/:id', userController.delete)
 
 export default route
