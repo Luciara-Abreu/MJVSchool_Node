26548e0878e2d70fbfdbba5ff0de782620da236e
Luciara-Abreu <luciara.abreu@gmail.com>
2023-04-03 12:17:54 -0300

    User controller

--- Agenda_Aniversariante/src/controllers/UserController.ts
+++ Agenda_Aniversariante/src/controllers/UserController.ts
@@ -1,30 +1,54 @@
 import { Request, Response } from 'express'
 import CreateUserService from '../sercive/userServices/createUserService'
 import ListAllUserService from '../sercive/userServices/listAllUserService'
-
+import ListOneUserService from '../sercive/userServices/listOneUserService'
+import updateUserService from '../sercive/userServices/updateUserService'
+import DeleteUserService from '../sercive/userServices/deleteUserService'
 
 class UserController {
-    public async createUser(req: Request, res: Response): Promise<Response> {
-
-    const { name, birthDate, sexualOrientation, email, lastEmail, fone, avatar } = req.body
-    const addneWUser = new CreateUserService()
-    const thisUser = await addneWUser.execute({
-      name,
-      birthDate,
-      sexualOrientation,
-      email,
-      lastEmail,
-      fone,
-      avatar
-    })
-  return res.json(thisUser)
+  public async createUser(req: Request, res: Response) {
+    try {
+      const thisUser = await CreateUserService.execute(req.body)
+      res.status(200).send({ message: 'Usuário add com sucesso! ', thisUser })
+    } catch (error: any) {
+      res.status(400).send({ message: error.message })
+    }
   }
 
   public async listAllUsers(req: Request, res: Response) {
-    const listUsers = new ListAllUserService()
-    const showUsers = await listUsers.execute()
-    //console.log('userController==> ', showUsers)
-    return res.json(showUsers)
+    try {
+      const showUsers = await ListAllUserService.execute()
+      res.status(200).send({ message: 'Lista de Usuários ==>  ', showUsers })
+    } catch (error: any) {
+      res.status(400).send({ message: error.message })
+    }
+  }
+
+  public async listOneUser(req: Request, res: Response) {
+    try {
+      const user = ListOneUserService.execute(req.params.id)
+      res.status(200).send({ message: `Id pesquisado  ${req.params.id} ==>`, user })
+    } catch (error: any) {
+      res.status(400).send({ message: error.message })
+    }
+  }
+
+  public async updateUser(req: Request, res: Response) {
+    try {
+      const user = updateUserService.execute(req.params.id, req.body)
+      res.status(200).send({ message: `Usuário atualizado com sucesso! ==> `, user })
+    } catch (error: any) {
+      res.status(400).send({ message: error.message })
+    }
+  }
+
+  public async delete(req: Request, res: Response) {
+    try {
+      DeleteUserService.execute(req.params.id)
+      res.status(200).send({ message: 'Usuário deletado com sucesso!' })
+    } catch (error: any) {
+      res.status(400).send({ message: error.message })
+    }
   }
 }
 
