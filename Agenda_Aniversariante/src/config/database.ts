4bd30c7adbdfa0bd11a1dabb10b538a3e51e6198
Luciara-Abreu <luciara.abreu@gmail.com>
2023-04-05 16:48:22 -0300

    new database

---
+++ Agenda_Aniversariante/src/config/database.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+mongoose.set('strictQuery', false)
+
+const databaseUrl = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/apiAniversariantes'
+const conectionDb = mongoose.connect(databaseUrl)
+
+export default conectionDb
