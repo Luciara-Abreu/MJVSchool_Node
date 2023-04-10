d905e908830d8416b852fcc321ba9d0d073d8c7e
Luciara-Abreu <luciara.abreu@gmail.com>
2023-04-10 15:39:33 -0300

    Path Mapping

--- Agenda_Aniversariante/src/config/auth.ts
+++ Agenda_Aniversariante/src/config/auth.ts
@@ -1,14 +1,11 @@
+import * as dotenv from 'dotenv'
+
+const secretMD5 = process.env.MD5_HASH
+console.log('secret here ===> ',secretMD5)
+
 export default {
   jwt: {
-    secret: '263c8651eb9b7183a3554008aaf05df0',
+    secret: '93c9f455fcac913b3956179f662197f3',
     expiresIn: '1d',
   },
 }
-/*
-export default {
-  jwt: {
-    secret: process.env.APP_SECRET,
-    expiresIn: '1d',
-  },
-};
-*/
