7a9d367246f952b68517166732008d58a9be9044
Luciara-Abreu <luciara.abreu@gmail.com>
2023-04-13 17:23:41 -0300

    add interface

---
+++ devschool-node/semana5.6/exercicio/src/interface/product.interface.ts
@@ -0,0 +1,12 @@
+
+
+interface IProd {
+  description: string,
+  img: string,
+  price:  number,
+  quantity:  number,
+  createdAt: string | Date
+  updatedAt: string | Date
+}
+
+export default IProd
