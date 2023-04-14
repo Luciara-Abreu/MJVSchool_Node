986314611082ada99b8c51af9b11e9338249244b
Luciara-Abreu <luciara.abreu@gmail.com>
2023-04-14 19:15:01 -0300

    update

--- devschool-node/semana5.6/exercicio/src/models/adm.model.ts
+++ devschool-node/semana5.6/exercicio/src/models/adm.model.ts
@@ -5,12 +5,10 @@ import IAdm from '../interface/adm.interface'
 export const admSchema = new Schema<IAdm>({
   name: {
     type: String,
-    unique: true,
     required: true,
   },
   email: {
     type: String,
-    unique: true,
     required: true,
   },
   password: {
