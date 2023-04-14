36fc0373c7a7a3d8ccb9ddf62a6df6af811c15ef
Luciara-Abreu <luciara.abreu@gmail.com>
2023-04-14 20:22:01 -0300

    using interface inthe repository adm

--- Agenda_Aniversariante/src/repository/adm.repository.ts
+++ Agenda_Aniversariante/src/repository/adm.repository.ts
@@ -1,51 +1,38 @@
-import { IAdm } from "../interfaces/adm.interface"
-import Adm from "../models/adm.model"
+import { IAdm } from '../interfaces/adm.interface'
+import Adm from '../models/adm.model'
 
 class AdmRepository {
+  getAll() {
+    return Adm.find()
+  }
 
-getAll(){
-  return Adm.find()
-}
+  getById(_id: string) {
+    return Adm.findOne({ _id: _id })
+  }
 
-getById(id:string){
-return Adm.findOne({_id: id})
-}
+  getByName(name: string) {
+    return Adm.findOne({ name: name })
+  }
 
-getByName(name:string){
-return Adm.findOne({name: name})
-}
+  getByBirthDate(birthDate: string) {
+    return Adm.findOne({ birthDate: birthDate })
+  }
 
-getByBirthDate(birthDate:string){
-return Adm.findOne({birthDate: birthDate})
-}
+  getByEmail(email: string) {
+    return Adm.findOne({ email: email })
+  }
 
-getByEmail(email:string){
-return Adm.findOne({email: email})
-}
+  create(adm: IAdm) {
+    return Adm.create(adm)
+  }
 
-/*create(adm: typeof Adm){
-  return Adm.create(adm)
-}*/
-create({ name, birthDate, sexualOrientation, email, fone, avatar, password}: IAdm){
-  return Adm.create({ 
-    name, 
-    birthDate,
-    sexualOrientation,
-    email,
-    fone,
-    avatar,
-    password        
-  })
-}
+  update(_id: string, adm: Partial<IAdm>) {
+    return Adm.updateOne({ _id: _id }, { $set: adm })
+  }
 
-update(id:string, adm: Partial<typeof Adm>){
-  return Adm.updateOne({_id:id}, {$set:adm})
-}
-
-remove(id:string){
-  return Adm.deleteOne({_id:id})
-}
-
+  remove(id: string) {
+    return Adm.deleteOne({ _id: id })
+  }
 }
 
 export default new AdmRepository()
